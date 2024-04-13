b283d1993e6fab6ec0ae6ad40c493cebe5ae7700
Geisiane Alves <101260452+GeisianeAlves@users.noreply.github.com>
2024-04-13 14:51:13 -0300

    index + saiba mais + barra de navegação

--- js/speaking.js
+++ js/speaking.js
@@ -1,139 +1,109 @@
-// Declaração variáveis
-const question = document.querySelector("#question");
-const answersBox = document.querySelector("#answers-box");
-const quizzContainer = document.querySelector("#quizz-container");
-const scoreContainer = document.querySelector("#score-container");
-const letters = ["a", "b"];
-let points = 0;
-let actualQuestion = 0;
 
-const questions = [
-  {   
-    "question": "Do you live in the town centre?  ",
-    "answers": [     
-      { "answer": "My apartment’s quite small.", "correct": false },
-      { "answer": "No, my apartment’s about five kilometres from here.", "correct": true }
-    ]  },
-  {
-    "question": "Right. Do you live in a house or an apartment?"  , 
-    "answers": [
-      { "answer": "An apartment. It’s quite small, just one bedroom. ", "correct": true },
-      { "answer": "My house is very expensive.",  "correct": false  }
-    ] },
-  {
-    "question": "Do you have a garden?" ,
-    "answers": [
-      { "answer": "My apartment’s very modern." , "correct": false },
-      { "answer": "No, but the view is beautiful.",  "correct": true }
-    ] },
-  {
-    "question": "What do you do? ",
-    "answers": [
-      { "answer": "I’m a sales assistant. I work in a shop.",  "correct": true },
-      {  "answer": "I work in Washington.", "correct": false }
-    ] },
-  {
-    "question": "When do you finish work? ",
-    "answers": [
-      { "answer": "I get up at quarter to six.", "correct": false },
-      { "answer": "I finish work at six.", "correct": true }
-  ]}
-  ]
+let score = 0;
+const scoreDisplay = document.getElementById('score');
+const scoreDisplay2 = document.getElementById('score2');
+const scoreDisplay3 = document.getElementById('score3');
+const scoreDisplay4 = document.getElementById('score4');
+const scoreDisplay5 = document.getElementById('score5');
+const scoreDisplay6 = document.getElementById('score6');
 
-function init() {
-  createQuestion(0);
-}
-
-function createQuestion(i) {
-  const oldButtons = answersBox.querySelectorAll("button");
-  oldButtons.forEach(function(btn) {
-    btn.remove();
-  });
-
-  // Alterar o texto da pergunta
-  const questionText = question.querySelector("#question-text");
-  const questionNumber = question.querySelector("#question-number");
-  questionText.textContent = questions[i].question;
-  questionNumber.textContent = "question " + (i + 1) + " of 5";
-
-  questions[i].answers.forEach(function(answer, i) {
-
-    const answerTemplate = document.querySelector(".answer-template").cloneNode(true);
-
-    const letterBtn = answerTemplate.querySelector(".btn-letter");
-    const answerText = answerTemplate.querySelector(".question-answer");
-    letterBtn.textContent = letters[i];
-    answerText.textContent = answer['answer'];
-
-    answerTemplate.setAttribute("correct-answer", answer["correct"]);
-    answerTemplate.classList.remove("hide");
-    answerTemplate.classList.remove("answer-template");
-    answersBox.appendChild(answerTemplate);
-
-    answerTemplate.addEventListener("click", function() {
-      checkAnswer(this);
-    });
-  });
-  actualQuestion++;
-}
-
-
-function checkAnswer(btn) {
-  const buttons = answersBox.querySelectorAll("button");
-  buttons.forEach(function(button) {
-
-    if(button.getAttribute("correct-answer") === "true") {
-      button.classList.add("correct-answer");
-      if(btn === button) {
-        points++;
-      }
+function handleOptionClick(option) {
+    if (option.classList.contains('correct')) {
+        score += 100;
+        scoreDisplay.textContent = `Score: ${score}`;
+        scoreDisplay2.textContent = `Score: ${score}`;
+        scoreDisplay3.textContent = `Score: ${score}`;
+        scoreDisplay4.textContent = `Score: ${score}`;
+        scoreDisplay5.textContent = `Score: ${score}`;
+        scoreDisplay6.textContent = `Score: ${score}`;
+        option.style.color = 'green';
+        // esconde a resposta certa
+        setTimeout(() => {
+            option.style.display = 'none';
+        }, 1500); 
     } else {
-      button.classList.add("wrong-answer");
+        option.style.color = 'red';
+        // esconde a resposta errada
+        setTimeout(() => {
+            option.style.display = 'none';
+        }, 1500); 
     }
-  });
-  nextQuestion();
+   
 }
+// todas as partes
+const parts = document.querySelectorAll('[id^="part-"]');
 
-
-function nextQuestion() {
-  setTimeout(function() {
-    if(actualQuestion >= questions.length) {
-      showSucccessMessage();
-      return;
-    }
-    createQuestion(actualQuestion);
-  }, 800);
-}
-
-
-function showSucccessMessage() {
-
-  hideOrShowQuizz();
-  const score = ((points / questions.length) * 100).toFixed(2);
-  const displayScore = document.querySelector("#display-score span");
-  displayScore.textContent = score.toString();
-
-  const correctAnswers = document.querySelector("#correct-answers");
-  correctAnswers.textContent = points;
-
-  const totalQuestions = document.querySelector("#questions-qty");
-  totalQuestions.textContent = questions.length;
-
-}
-
-function hideOrShowQuizz() {
-  quizzContainer.classList.toggle("hide");
-  scoreContainer.classList.toggle("hide");
-}
-
-const restartBtn = document.querySelector("#restart");
-
-restartBtn.addEventListener("click", function() {
-  actualQuestion = 0;
-  points = 0;
-  hideOrShowQuizz();
-  init();
+parts.forEach(part => {
+    part.querySelectorAll('p').forEach(option => {
+        option.addEventListener('click', function() {
+            handleOptionClick(option);
+        });
+    });
+  
 });
 
+//função para o cronômetro
+function formatTime(seconds) {
+    const minutes = Math.floor(seconds / 60);
+    const secondsLeft = seconds % 60;
+    const formattedMinutes = String(minutes).padStart(2, '0');
+    const formattedSeconds = String(secondsLeft).padStart(2, '0');
+    return `${formattedMinutes}:${formattedSeconds}`;
+}
 
-init();
+function updateTimerDisplay(timerElement, remainingTime) { 
+    timerElement.textContent = ` ${formatTime(remainingTime)}`;
+}
+
+function startTimer(durationInSeconds) {
+    let remainingTime = durationInSeconds;
+
+    const timerElement = document.getElementById('timer');
+    const timerElement2 = document.getElementById('timer2');
+    const timerElement3 = document.getElementById('timer3');
+    const timerElement4 = document.getElementById('timer4');
+    const timerElement5 = document.getElementById('timer5');
+    const timerElement6 = document.getElementById('timer6');
+
+    updateTimerDisplay(timerElement, remainingTime);
+    updateTimerDisplay(timerElement2, remainingTime);
+    updateTimerDisplay(timerElement3, remainingTime);
+    updateTimerDisplay(timerElement4, remainingTime);
+    updateTimerDisplay(timerElement5, remainingTime);
+    updateTimerDisplay(timerElement6, remainingTime);
+
+    const interval = setInterval(() => {
+        remainingTime--;
+        updateTimerDisplay(timerElement, remainingTime);
+        updateTimerDisplay(timerElement2, remainingTime);
+        updateTimerDisplay(timerElement3, remainingTime);
+        updateTimerDisplay(timerElement4, remainingTime);
+        updateTimerDisplay(timerElement5, remainingTime);
+        updateTimerDisplay(timerElement6, remainingTime);
+
+        if (remainingTime <= 0) {
+            clearInterval(interval); 
+            timerElement.textContent = "Time out";
+            timerElement2.textContent = "Time out";
+            timerElement3.textContent = "Time out";
+            timerElement4.textContent = "Time out";
+            timerElement5.textContent = "Time out";
+            timerElement6.textContent = "Time out";
+            endGame()
+        }
+    }, 1000); 
+           
+}
+
+// cronômetro com uma duração de 1 minuto (60 segundos)
+startTimer(60);
+
+function endGame() {
+    const options = document.querySelectorAll('.palavras p');
+    // const tema = document.querySelectorAll('#tema');
+
+    // tema.style.display = 'none';
+    options.forEach(option => {
+        option.style.display = 'none';
+    });
+}
